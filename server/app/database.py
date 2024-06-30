33cbf80c9e812feb4e29dd773fa2dab6c4cef68b
HLrahul <rahuls10org@gmail.com>
2024-06-30 15:47:05 +0530

    chore: enhances

--- server/app/database.py
+++ server/app/database.py
@@ -2,10 +2,16 @@
 Connect to DB
 """
 
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/postgres"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
